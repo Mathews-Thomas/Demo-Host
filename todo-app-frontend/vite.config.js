353c72656ec1ef06aa1fd7f31f152d681e630a5c
Mathews Thomas <mathewsxo@gmail.com>
2024-10-04 15:27:45 +0530

    new

--- todo-app-frontend/vite.config.js
+++ todo-app-frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',  // Backend server address
+        target: 'https://demo-backend-4vur.onrender.com',  // Backend server address
         changeOrigin: true,
       },
     },
